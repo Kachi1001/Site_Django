6e15488d2a89a055a01ba50f2f52f6a84a41c531
Kachi1001 <felipekaim234@gmail.com>
2024-08-01 15:33:48 -0300

    concertos de config

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
 git pull
-<<<<<<< HEAD
-python manage.py runserver localhost:8000
-=======
-python manage.py runserver
->>>>>>> a66630eaa4b642fd9cec5cf077b50c412618a2ea
+python manage.py runserver localhost:8001
