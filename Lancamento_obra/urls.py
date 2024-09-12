a6314b51d683e8cdd05c5db12378181893adc727
Felipe Kaim <felipekaim234@gmail.com>
2024-09-12 17:47:53 -0300

    +grafico

--- Lancamento_obra/urls.py
+++ Lancamento_obra/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("tabelas/<str:table>", views.tabelas, name= "tabelas"),
     path("consultas/<str:table>", views.consultas, name= "consultas"),
     path("lancamentos/<str:type>", views.lancamentos, name= "lancamentos"),
+    path("graficos/<str:type>", views.graficos, name= "graficos"),
 ]
