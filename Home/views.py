9270f823f6380ee7e2ee81810ac2f0e75797e614
Kachi1001 <felipekaim234@gmail.com>
2024-10-17 13:25:34 -0300

    configurado para identificar a next e redirecionar apos exito

--- Home/views.py
+++ Home/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth import logout as logout_django
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import AccessMixin
 from .models import *
 
 app = __name__.split('.')[0]
@@ -11,12 +12,13 @@ app = __name__.split('.')[0]
 def login(request):
     if request.method == "GET":
         logout_django(request)
-        return render(request, f'{app}/login.html')
+        return render(request, f'{app}/login.html', {'next':request.GET.get('next', '/')})
     elif request.method == "POST":
-        user = authenticate(username= request.POST.get('nome'), password= request.POST.get('senha'))
+        user = authenticate(username= request.POST['nome'], password= request.POST['senha'])
         if user:
             login_django(request, user)
-            return redirect('/')
+            print(request.POST)
+            return redirect(request.POST.get('next', '/'))
         else:
             return render(request, f'{app}/login.html', {"login": request.POST.get('nome')})
 
