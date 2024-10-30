19149c81d7b945a424c5e64e2971f34dc2e08e54
Kachi1001 <felipekaim234@gmail.com>
2024-10-30 11:42:58 -0300

    Verificando antes de fazer request

--- Reservas/static/js/Reservas.js
+++ Reservas/static/js/Reservas.js
@@ -136,14 +136,21 @@ function Reserva_rapida() {
         ).val()}.jpg`;
     });
     $("#reserva_rapida-submit").click(function () {
+        const responsavel = $("#reserva_rapida-responsavel").val()
+        const descricao = $("#reserva_rapida-descricao").val()
+        const data = $("#reserva_rapida-data").val()
+        if (responsavel == '' || data == '') {
+            toasts('warning',{'method':'Reserva','message':'Falta alguma informação revise!!'})
+            return
+        }
         let reservas = [];
         let inicio = parseInt($("#reserva_rapida-sel1").val());
         let fim = parseInt($("#reserva_rapida-sel2").val());
         for (i = inicio; i < fim; i++) {
             reservas.push({
                 hora: horarios[i],
-                responsavel: $("#reserva_rapida-responsavel").val(),
-                descricao: $("#reserva_rapida-descricao").val(),
+                responsavel: responsavel,
+                descricao: descricao,
             });
         }
         parametro = {
