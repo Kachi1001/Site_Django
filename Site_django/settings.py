9968dca16c18df00b7fdca1d9f7d7fa250a3a090
Kachi1001 <felipekaim234@gmail.com>
2024-11-06 16:53:00 -0300

    -whitenoise

--- Site_django/settings.py
+++ Site_django/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Site_django.urls'
