6510f1c37fd2fff87d463ccf35755059354d8415
Kachi1001 <felipekaim234@gmail.com>
2024-12-05 11:41:35 -0300

    aa

--- Home/views.py
+++ Home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
-from django.contrib.auth import logout as logout_django
+from django.contrib.auth import authenticate, login as login_django, logout as logout_django
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import AccessMixin
 from .models import *
+
+import requests
+from decouple import config
 
 app = __name__.split('.')[0]
 
@@ -14,10 +14,26 @@ def login(request):
         logout_django(request)
         return render(request, f'{app}/login.html', {'next':request.GET.get('next', '/')})
     elif request.method == "POST":
-        user = authenticate(username= request.POST['nome'], password= request.POST['senha'])
+        username = request.POST['nome']
+        password = request.POST['senha']
+        user = authenticate(username= username, password=password)
         if user:
             login_django(request, user)
-            return redirect(request.POST.get('next', '/'))
+            api_url = f"{config('API')}token"
+            api_response = requests.post(api_url, json={
+                'username': username,
+                'password': password
+            })
+            if api_response.status_code == 200:
+                token = api_response.json().get('access')
+                request.session['api_token'] = token  # Salva o token na sessão
+                return redirect(request.POST.get('next', '/'))
+            else:
+                return render(request, f'{app}/login.html', {
+                    "login": username,
+                    "error": "Erro ao autenticar na API externa.",
+                    "next": request.GET.get('next', '/')
+                })
         else:
             return render(request, f'{app}/login.html', {"login": request.POST.get('nome'), 'next':request.GET.get('next', '/')})
 
@@ -33,3 +49,4 @@ def teste(request):
 from django.http import HttpResponse
 def status(request):
     return HttpResponse("Estamos online!!")
+
