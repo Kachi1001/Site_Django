9d69c3b1e0a777c5818893d65cc5a0668648ec06
Kachi1001 <felipekaim234@gmail.com>
2024-12-27 16:26:18 -0300

    limpeza

--- Obra/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import User
-from rolepermissions.admin import RolePermissionsUserAdminMixin
-from rolepermissions.roles import get_user_roles, assign_role, remove_role
