47b7ed1d2908e523a02ab4bf2ab021f664a31159
Kachi1001 <felipekaim234@gmail.com>
2024-12-27 16:22:28 -0300

    tirando inutil

--- Curriculos/urls.py
+++ Curriculos/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("cadastros/<str:resource>", views.cadastros, name=views.app+"_cadastros"),
     path("tabelas/<str:resource>", views.tabelas, name=views.app+"_tabelas"),
     path("consultas/<str:resource>", views.consultas, name=views.app+"_consultas"),
-    path("lancamentos/<str:resource>", views.lancamentos, name=views.app+"_lancamentos"),
+    # path("lancamentos/<str:resource>", views.lancamentos, name=views.app+"_lancamentos"),
     path("graficos/<str:resource>", views.graficos, name=views.app+"_graficos"),
-    path("app", views.app_menu, name=views.app+"_app"),
 ]
