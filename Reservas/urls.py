0f03af7c92a62560abbedc5a7935a37ab93342b8
kachii1001 <felipekaim234@gmail.com>
2026-06-29 17:55:21 -0300

    teste de redirect?

--- Reservas/urls.py
+++ Reservas/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
+
+# URL do novo site para onde todas as rotas de /Reservas serao redirecionadas.
+NOVO_SITE_URL = "https://salas.mke.app.br/"
 
 urlpatterns = [
-    path("", views.index,  name=views.app),
-
-    path("sala/registros", views.sala_registros, name= "sala_registros"), 
-    path('sala/<str:sala>', views.sala, name='sala'),    
-]  
+    # Captura todas as rotas de /Reservas/* e redireciona para o novo site.
+    # permanent=True -> 301 (redirecionamento permanente). Use False para 302.
+    re_path(r"^.*$", RedirectView.as_view(url=NOVO_SITE_URL, permanent=True)),
+]
