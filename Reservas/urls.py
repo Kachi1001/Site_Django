f9f517e300b87524c117abe1fae43a9a3e6057c0
kachii1001 <felipekaim234@gmail.com>
2026-06-30 07:33:20 -0300

    novo teste

--- Reservas/urls.py
+++ Reservas/urls.py
@@ -4,8 +4,13 @@ from django.views.generic.base import RedirectView
 # URL do novo site para onde todas as rotas de /Reservas serao redirecionadas.
 NOVO_SITE_URL = "https://salas.mke.app.br/"
 
+_redirect = RedirectView.as_view(url=NOVO_SITE_URL, permanent=True)
+
 urlpatterns = [
-    # Captura todas as rotas de /Reservas/* e redireciona para o novo site.
+    # URL nomeada "Reservas" usada pela home em {% url 'Reservas' %}.
+    # Sem este name a home quebra com NoReverseMatch (erro 500).
+    path("", _redirect, name="Reservas"),
+    # Captura todas as demais rotas de /Reservas/* e redireciona para o novo site.
     # permanent=True -> 301 (redirecionamento permanente). Use False para 302.
-    re_path(r"^.*$", RedirectView.as_view(url=NOVO_SITE_URL, permanent=True)),
+    re_path(r"^.*$", _redirect),
 ]
